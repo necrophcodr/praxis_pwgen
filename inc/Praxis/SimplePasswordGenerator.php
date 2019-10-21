95b60370485af01a5e35da8f005ce99dee1371e8
necrophcodr <nc@scalehost.eu>
2019-10-21 14:06:28 +0200

    fix: increase number of characters available.

--- inc/Praxis/SimplePasswordGenerator.php
+++ inc/Praxis/SimplePasswordGenerator.php
@@ -5,7 +5,7 @@ include_once "PasswordGenerator.php";
 class SimplePasswordGenerator {
     use \Praxis\Traits\PasswordGenerator;
 
-    public $src = "ABCDEFGabcdefg1234567890,.-'!#¤%&/()=?@£\${[]}|~*";
+    public $src = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,.-'!#¤%&/()=?@£\${[]}|~*";
 
     public function Generate(int $length) {
         return $this->_generate($this->src, $length);
--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
-
 include_once "inc/Praxis/SimplePasswordGenerator.php";
 
 $pwgen = new Praxis\Helpers\SimplePasswordGenerator();
 
 printf( "\$pwgen->Generate(10): %s<br/>", $pwgen->Generate(10) );
 
-$pwgen->src = "ABCDEFGabcdefg1234567890";
+$pwgen->src = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-printf( "\$pwgen->src = 'ABCDEFGabcdefg1234567890';<br/>" );
+printf( "\$pwgen->src = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';<br/>" );
 printf( "\$pwgen->Generate(10): %s<br/>", $pwgen->Generate(10) );
-
 ?>
