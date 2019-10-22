5837bd2108e5ef1fe553a32a06daf56a55d5ed68
necrophcodr <nc@scalehost.eu>
2019-10-22 13:37:32 +0200

    fix: index used incorrect display values.

--- index.php
+++ index.php
@@ -9,12 +9,12 @@ include_once "inc/Praxis/PasswordGenerator.php";
 
 $pwgen = new Praxis\Helpers\SimplePasswordGenerator();
 
-printf( "\$pwgen->Generate(16): %s<br/>", $pwgen->Generate(10) );
+printf( "\$pwgen->Generate(10): %s<br/>", $pwgen->Generate(10) );
 
 $pwgen->src = ["0123456789","abcdefghijklmnopqrstuvwxyz","ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 
 printf( '$pwgen->src = ["0123456789","abcdefghijklmnopqrstuvwxyz","ABCDEFGHIJKLMNOPQRSTUVWXYZ"];<br/>' );
-printf( "\$pwgen->Generate(16): %s<br/>", $pwgen->Generate(10) );
+printf( "\$pwgen->Generate(10): %s<br/>", $pwgen->Generate(10) );
 
 $vlpwgen = new Praxis\Helpers\VariableLengthPasswordGenerator();
 
