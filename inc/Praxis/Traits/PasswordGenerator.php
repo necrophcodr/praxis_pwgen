f33a623f4ed8aced0cba84f987e49638641bd3c4
necrophcodr <nc@scalehost.eu>
2019-10-22 13:33:19 +0200

    fix: use correct groups.

--- inc/Praxis/Traits/PasswordGenerator.php
+++ inc/Praxis/Traits/PasswordGenerator.php
@@ -15,9 +15,9 @@ trait PasswordGenerator {
             $next_group = random_int( 0, count($in)-1 );
             if( $length-mb_strlen($pwd) === count($groups) && count($groups) > 0) {
                 foreach( $groups as $group => $used ) {
-                    $next_byte = random_int( 0, mb_strlen($in[$next_group])-1 );
-                    $groups[$next_group]=1;
-                    $pwd = $pwd . $in[$next_group][$next_byte];
+                    $next_byte = random_int( 0, mb_strlen($in[$group])-1 );
+                    $groups[$group]=1;
+                    $pwd = $pwd . $in[$group][$next_byte];
                 }
             }
             $next_byte = random_int( 0, mb_strlen($in[$next_group])-1 );
